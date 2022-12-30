c4bc7bc2f77a0d8c4f68620487f25a2dbfea2be1
JamesHou <houj0411@gmail.com>
2022-12-30 16:29:14 +0800

    plm/instruct: update todo works in run.sh

--- plm/instruct/README.md
+++ plm/instruct/README.md
@@ -3,6 +3,6 @@ WIP
 
 # Records of Results (MIMIC3-FULL, macro-f1, micro-f1)
 - FLAN-T5-bese-FULL: 6.18%, 38.19%
-- T5.1.1-base-FULL: 
+- T511-base-FULL: 
 - LONG-T5-base-FULL:
 
--- plm/instruct/run.sh
+++ plm/instruct/run.sh
@@ -14,10 +14,14 @@ python train.py google/t5-v1_1-base --dataset full --epoch 10 --maxlen 1024
 # https://huggingface.co/google/long-t5-tglobal-base
 # https://huggingface.co/google/long-t5-local-base
 # global better than local?
-# python train.py google/long-t5-tglobal-base --dataset full --epoch 10 --maxlen 1024
+# python train_long.py google/long-t5-tglobal-base --dataset full --epoch 10 --maxlen 1024
 
 # 4th
 # Stancld/longt5-tglobal-large-16384-pubmed-3k_steps
 # https://huggingface.co/Stancld/longt5-tglobal-large-16384-pubmed-3k_steps
-# python train.py Stancld/longt5-tglobal-large-16384-pubmed-3k_steps --dataset full --epoch 10 --maxlen 1024
+# python train_long.py Stancld/longt5-tglobal-large-16384-pubmed-3k_steps --dataset full --epoch 10 --maxlen 1024
+
+# 5th
+# google/long-t5-tglobal-large
+# (This is to compare with 4th model)
 
